ef1b339ba5e19a9204b73cd9bdeb57093edfaf50
mversionpenny <margot.selosse@gmail.com>
2016-11-03 16:11:19 +0100

    first-shot : first etl on our data

---
+++ first-shot.R
@@ -0,0 +1,16 @@
+# Data ware house #
+# Margot Selosse et Thu Nguyen #
+
+# http://data.europa.eu/euodp/fr/data/dataset/data_national-emissions-reported-to-the-unfccc-and-to-the-eu-greenhouse-gas-monitoring-mechanism-12
+
+#### Loading data ####
+data <- read.csv("D:/data-warehouse/UNFCCC_V19.csv", sep="\t")
+# levels(data$Sector_name)
+# levels(data$Sector_code)
+# 
+# which(typeof(data$Year) != integer)
+# levels(data$Notation)
+
+#### Deleting coloumns we are not interested in  : Unit and Format_name ####
+data <- data[,- c(4,10)]
+colnames(data)
