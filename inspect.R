717d9d90565926b4489fd5c87d7719e8bcc36cd5
Hoai Thu Nguyen <hoaithu.nguyen.26.4.92@gmail.com>
2016-11-05 15:15:19 +0100

    changed inspect.R

--- inspect.R
+++ inspect.R
@@ -1,6 +1,10 @@
 # Data ware house #
 # Margot Selosse et Thu Nguyen #
 # Just inspecting the data, changing nothing #
+list.of.packages <- c("data.table")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, repos = "http://cran.rstudio.com/")
+library(data.table)
 
 # http://data.europa.eu/euodp/fr/data/dataset/data_national-emissions-reported-to-the-unfccc-and-to-the-eu-greenhouse-gas-monitoring-mechanism-12
 #Margot
@@ -9,7 +13,7 @@
 setwd("C:/Users/Hoai Thu Nguyen/Dropbox/DM/BD/Projet")
 
 #### Loading data ####
-data <- read.csv("UNFCCC_V19.csv", sep="\t")
+data <- fread("UNFCCC_V19.csv", sep="\t", stringsAsFactors=T)
 
 #### Format_name ####
 levels(data$Format_name)
@@ -17,7 +21,8 @@ levels(data$Format_name)
 
 #### Unit ####
 levels(data$Unit)
-# Only one levels -> can remove this factor
+#[1] "Gg"                "Gg CO2 equivalent"
+#  -> can remove this factor
 
 #### Sector_name ####
 weird_sectors <- data[grepl("4\\(", data$Sector_name),] #extract the lines with weird sector name
@@ -27,7 +32,5 @@ not_zero$Sector_name <- factor(not_zero$Sector_name) # drop levels that don't ex
 not_zero$Pollutant_name <- factor(not_zero$Pollutant_name) 
 levels(not_zero$Sector_name) # All weird sectors have non-zero values
 levels(not_zero$Pollutant_name)
-# Only CH4, CO2, N2O ==> Not coherent at all because normally, the "All greenhouse gas" is the sum of all the gas
-# if one of the gas != 0, the "All green house gas" mustnot be 0 
-# number of lines != 0 with weird sector name is 2000 ~ 1% of the data -> can be considered negligeable
+# number of lines != 0 with weird sector name is ~5000 ~ 1% of the data -> can be considered negligeable
 # We can remove all the weird sectors but will have to recalculate the sums in the parent sectors !!!!
