0a6bdcdd6b02793b0b3dd5c77c7a8379f622a822
Hoai Thu Nguyen <hoaithu.nguyen.26.4.92@gmail.com>
2016-11-09 17:14:26 +0100

    complete first-shot.R, add clean_data.csv

--- first-shot.R
+++ first-shot.R
@@ -38,20 +38,60 @@ levels(data$Sector_code)[levels(data$Sector_code)=="4.E Biomass Burning"] <- "4.
 levels(data$Sector_code)[levels(data$Sector_code)=="-"] <- "4.F.1"
 data$Parent_sector_code[which(data$Sector_code=="4.F.1")] <- "4.F"
 
-## Replace the bad Sector_code of sector 3
+## Remove 3.1
 levels(data$Parent_sector_code)[levels(data$Parent_sector_code)=="3.1"] <- "3"
 data <- subset(data, Sector_code != "3.1")
 data$Sector_code <- factor(data$Sector_code)
 
-#### Inspecting sector_name ####
-#sector_name <- as.character(data$Sector_name)
-for (i in 1:length(levels(data$Sector_name))){
-  name = unlist(strsplit(levels(data$Sector_name)[i], split=" - "))
-  if (length(name) != 2){
-    print(levels(data$Sector_name)[i])
+#### Remove sector code from Sector_name ####
+levels(data$Sector_name)[levels(data$Sector_name)=="- 4(IV)  Indirect N2O Emissions from Managed Soils"] <- "Indirect N2O Emissions from N Mineralization/Immobilization"
+sector_names <- levels(data$Sector_name)
+for (i in 1:length(sector_names)){
+  sector <- sector_names[i]
+  name = unlist(strsplit(sector, split="-"))
+  if (length(name) == 2){
+    levels(data$Sector_name)[levels(data$Sector_name)==sector] <- trimws(name[2])
+  }
+  if (length(name) < 2){
+    if (grepl("4\\(", sector))
+      levels(data$Sector_name)[levels(data$Sector_name)==sector] <- trimws(unlist(strsplit(sector, split="\\)"))[2])
+  }
+  if (length(name) > 2){
+    new_name <- paste(trimws(name[2]), '-', trimws(name[3]), sep='')
+    levels(data$Sector_name)[levels(data$Sector_name)==sector] <- new_name
   }
 }
 
-#### Deleting sector_code from sector_name ####
-sector_name <- sapply(levels(data$Sector_name), function(x) unlist(strsplit(x, split=" - "))[2])
-le
+#### Remove total + all green house gases ####
+data <- subset(data, Pollutant_name != "All greenhouse gases - (CO2 equivalent)")
+data$Pollutant_name <- factor(data$Pollutant_name)
+
+levels(data$Parent_sector_code)[levels(data$Parent_sector_code)=="Sectors/Totals_incl_incl"] <- ""
+data <- subset(data, !grepl("Sector", Sector_code))
+
+#### Creat new big sectors about international activities, bioamss and indirect CO2 ####
+levels(data$Sector_code)[levels(data$Sector_code)=="ind_CO2"] <- "7"
+levels(data$Parent_sector_code)[levels(data$Parent_sector_code)=="1.D"] <- ""
+levels(data$Sector_code)[levels(data$Sector_code)=="1.D.1"] <- "8"
+levels(data$Sector_code)[levels(data$Sector_code)=="1.D.1.a"] <- "8.A"
+levels(data$Sector_code)[levels(data$Sector_code)=="1.D.1.b"] <- "8.B"
+levels(data$Parent_sector_code)[levels(data$Parent_sector_code)=="1.D.1"] <- "8"
+levels(data$Sector_code)[levels(data$Sector_code)=="1.D.2"] <- "9"
+levels(data$Sector_code)[levels(data$Sector_code)=="1.D.3"] <- "10"
+
+#### Recompute the sum sector 4 and 1 ####
+for(i in which(data$Sector_code == "4.F")){
+  temp <- subset(data, Country == data$Country[i] & Pollutant_name == data$Pollutant_name[i] & Year == data$Year[i] & Parent_sector_code == "4.F")
+  data$emissions[i] <- sum(temp$emissions)  
+}
+for(i in which(data$Sector_code == "4")){
+  temp <- subset(data, Country == data$Country[i] & Pollutant_name == data$Pollutant_name[i] & Year == data$Year[i] & Parent_sector_code == "4")
+  data$emissions[i] <- sum(temp$emissions)  
+}
+for(i in which(data$Sector_code == "1")){
+  temp <- subset(data, Country == data$Country[i] & Pollutant_name == data$Pollutant_name[i] & Year == data$Year[i] & Parent_sector_code == "1")
+  data$emissions[i] <- sum(temp$emissions)  
+}
+
+#### Save cleaned data ####
+write.table(data, "clean_data.csv", row.names = F, append = F, sep = ";") 
